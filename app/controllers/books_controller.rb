44ddc6e3fc577308b005059b39d33f1bf682ef9b
sakiponjuice <saki.pon.juice0922@gmail.com>
2020-11-21 12:27:19 +0000

    [Add] バリデーションとサクセスメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,20 +16,25 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-     redirect_to book_path(@book.id)
+      redirect_to book_path(@book.id), notice: "You have created book successfully."
     else
+      @books = Book.all
       render :index
     end
   end
 
   def edit
     @book = Book.find(params[:id])
+    if @book.user!=current_user
+      redirect_to books_path
+    else
+    end
   end
 
   def update
      @book = Book.find(params[:id])
      if @book.update(book_params)
-       redirect_to book_path(@book.id)
+       redirect_to book_path(@book.id), notice: "You have updated book successfully."
      else
        render :edit
      end
