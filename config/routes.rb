a730602f3787f99892705fdd16e0fed917a0b148
sakiponjuice <saki.pon.juice0922@gmail.com>
2020-12-17 05:08:11 +0000

    [Finish] コメント機能追加、応用課題3終了

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :users
   resources :books do
   resource :favorites, only: [:create, :destroy]
+  resources :book_comments, only: [:create, :destroy]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
