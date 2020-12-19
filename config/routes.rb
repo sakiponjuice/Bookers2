e8337c018152bd2204800733c431c358a1dd5d7e
sakiponjuice <saki.pon.juice0922@gmail.com>
2020-12-19 03:14:55 +0000

    [Finish] 検索機能、応用課題5

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,6 @@ Rails.application.routes.draw do
   resources :users, only: :show
   get 'users/:id/follower' => 'users#follower', as: 'user_follower'
   get 'users/:id/followed' => 'users#followed', as: 'user_followed'
+  get 'search/search' => 'search#search', as: 'search'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
